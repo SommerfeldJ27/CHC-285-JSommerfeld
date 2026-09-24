31de39cb17bc2970df97fcc84dd908e6a2f42167
SommerfeldJ27 <sommerfeldj27@chcstudent.com>
2026-09-24 09:18:29 -0400

    Update triangle.java

--- src/triangle.java
+++ src/triangle.java
@@ -13,14 +13,14 @@ public class triangle
       System.out.print("Enter Side 3: ");
       int side3 = sc.nextInt();
       
-      if (Math.pow(side1,2) + Math.pow(side2,2) == Math.pow(side3,2)) {
-         System.out.println("Right Triangle");   
-      } if (Math.pow(side3,2) + Math.pow(side2,2) == Math.pow(side1,2)) {
-         System.out.println("Right Triangle");   
-      } if (Math.pow(side1,2) + Math.pow(side3,2) == Math.pow(side2,2)) {
-         System.out.println("Right Triangle");   
-      } else {
-        System.out.println("Not Right Triangle");
+      if
+      (Math.pow(side1,2) + Math.pow(side2,2) == Math.pow(side3,2) || 
+      (Math.pow(side3,2) + Math.pow(side2,2) == Math.pow(side1,2) || 
+      (Math.pow(side1,2) + Math.pow(side3,2) == Math.pow(side2,2))))
+         System.out.println("Right Triangle");
+      else 
+      {
+         System.out.println("Not Right Triangle");
       }
       sc.close();
    }
